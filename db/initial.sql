440275955b91e03995ba83d8e8af5c0818361e6b
yeatea <harison.4663@gmail.com>
2018-06-30 17:57:08 +0900

    Nginx + php + mysql 環境設定(docker)

---
+++ db/initial.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `samples` (
+  `sample_id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`sample_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
